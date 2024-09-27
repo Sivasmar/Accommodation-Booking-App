b723fbc032a77bdeefc4a3105ec9caaef72c5268
siva vempuluru <vempulurusiva@gmail.com>
2024-09-27 20:08:32 +0530

    project2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {
   Route,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Hotel from './pages/hotel/Hotel';
+import List from './pages/list/List';
 
 
 function App() {
@@ -14,7 +16,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-       
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
       </Routes>
     </BrowserRouter>
   );
